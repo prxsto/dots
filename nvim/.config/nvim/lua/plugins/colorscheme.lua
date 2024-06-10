4070ef67fe86e755d72c05acc2ef285831ee34cc
prxsto <15711032+prxsto@users.noreply.github.com>
2024-06-09 17:16:35 -0700

    Add ayu theme, waybar progress

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,25 @@
 return {
-	"folke/tokyonight.nvim",
+	"Shatur/neovim-ayu",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "moon",
-			light_style = "day",
-			transparent = true,
-			terminal_colors = true,
+		require("ayu").setup({
+			mirage = true,
+			terminal = true,
+			overrides = {
+				Normal = { bg = "None" },
+				ColorColumn = { bg = "None" },
+				SignColumn = { bg = "None" },
+				Folded = { bg = "None" },
+				FoldColumn = { bg = "None" },
+				CursorLine = { bg = "None" },
+				CursorColumn = { bg = "None" },
+				WhichKeyFloat = { bg = "None" },
+				VertSplit = { bg = "None" },
+			},
 		})
 	end,
 	init = function()
-		vim.cmd("colorscheme tokyonight-moon")
+		vim.cmd("colorscheme ayu")
 	end,
 }
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -3,7 +3,6 @@ return {
 	event = "VeryLazy",
 	init = function()
 		vim.o.timeout = true
-		vim.o.timeout = 300
 	end,
 	opts = {
 		defaults = {
