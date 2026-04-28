21c9a9d9178e31cb3e942252614661570e3fee87
Prxsto <15711032+prxsto@users.noreply.github.com>
2026-04-28 16:49:29 -0500

    rip treesitter o7

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,67 +1,57 @@
--- filetype detection for hypr ecosystem config files
+-- treesitter.lua
 vim.filetype.add({
    pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 })
 
 return {
-   "nvim-treesitter/nvim-treesitter",
-   event = { "BufReadPre", "BufNewFile" },
-   build = ":TSUpdate",
-   dependencies = {
-      "windwp/nvim-ts-autotag",
-   },
-   config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
-
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-         highlight = {
-            enable = true,
-         },
-         -- enable indentation
-         indent = { enable = true },
-         -- enable autotagging (w/ nvim-ts-autotag plugin)
-         autotag = {
-            enable = true,
-         },
-         auto_install = true,
-         -- ensure these language parsers are installed
-         ensure_installed = {
-            "hyprlang",
-            "comment",
-            "c",
-            "go",
-            "json",
-            "javascript",
-            "typescript",
-            "tsx",
-            "yaml",
-            "html",
-            "css",
-            "prisma",
-            "markdown",
-            "markdown_inline",
-            "svelte",
-            "graphql",
-            "lua",
-            "vim",
-            "dockerfile",
-            "gitignore",
-            "query",
-            "vimdoc",
-            "python",
-            "rust",
-         },
-         incremental_selection = {
-            enable = true,
-            keymaps = {
-               init_selection = "<C-space>",
-               node_incremental = "<C-space>",
-               scope_incremental = false,
-               node_decremental = "<bs>",
+   {
+      "romus204/tree-sitter-manager.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+      config = function()
+         require("tree-sitter-manager").setup({
+            ensure_installed = {
+               "hyprlang",
+               "c",
+               "go",
+               "json",
+               "javascript",
+               "typescript",
+               "tsx",
+               "yaml",
+               "html",
+               "css",
+               "prisma",
+               "markdown",
+               "markdown_inline",
+               "svelte",
+               "graphql",
+               "lua",
+               "vim",
+               "dockerfile",
+               "gitignore",
+               "query",
+               "vimdoc",
+               "python",
+               "rust",
+               "tmux",
             },
-         },
-      })
-   end,
+            auto_install = true,
+            highlight = true,
+            border = "rounded",
+         })
+      end,
+   },
+   {
+      "windwp/nvim-ts-autotag",
+      event = { "BufReadPre", "BufNewFile" },
+      config = function()
+         require("nvim-ts-autotag").setup({
+            opts = {
+               enable_close = true,
+               enable_rename = true,
+               enable_close_on_slash = false -- auto close on </
+            },
+         })
+      end,
+   }
 }
